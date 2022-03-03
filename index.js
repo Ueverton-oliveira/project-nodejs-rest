b65b99860989565949fa6bef6ea6f71c56b0c723
Ueverton-oliveira <ueverton.souz@gmail.com>
2022-03-03 19:29:24 -0400

    feat: conect database

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao.js')
 
+conexao.connect(erro => {
+  if(erro) {
+    console.log(erro)
+  }else{
+    console.log('Conectado com sucesso!')
+  }
+})
 const app = customExpress()
 
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
